ee90313459bdfc3d1ed14b96c495ea37f02c2fb7
khasyah-fr <khasyahfr@gmail.com>
2025-05-05 13:22:04 +0700

    process train and val dataset

--- generate_dataframe_reference.py
+++ generate_dataframe_reference.py
@@ -16,13 +16,17 @@ df = pd.DataFrame({
     "is_duplicate": pd.Series(dtype='bool'),
 })
 
-ms_asl_json = pd.read_json("MSASL_test.json")
+ms_asl_train_json = pd.read_json("MSASL_train.json")
+ms_asl_val_json = pd.read_json("MSASL_val.json")
+ms_asl_test_json = pd.read_json("MSASL_test.json")
 
-rows = []
+train_rows = []
+val_rows = []
+test_rows = []
 
+# Process train rows
 counter = len(df)
-
-for _, row in ms_asl_json.iterrows():
+for _, row in ms_asl_train_json.iterrows():
     category = row["clean_text"]
 
     new_row = {
@@ -38,11 +42,60 @@ for _, row in ms_asl_json.iterrows():
         "is_valid": None, #TODOchecked
         "is_duplicate": None, #TODOchecked
     }
-    rows.append(new_row)
+    train_rows.append(new_row)
+    counter += 1
+
+df = pd.concat([df, pd.DataFrame(train_rows)], ignore_index=True)
+
+
+
+# Process val rows
+counter = len(df)
+for _, row in ms_asl_val_json.iterrows():
+    category = row["clean_text"]
+
+    new_row = {
+        "id": counter + 1,
+        "category": category,
+        "dataset_type": "MSASL",
+        "url": row["url"],
+        "dataset_split": "", #TODOsplit 
+        "frame_start": row["start"],
+        "frame_end": row["end"],
+        "fps": row["fps"],
+        "filename": f"{counter+1}.mp4",
+        "is_valid": None, #TODOchecked
+        "is_duplicate": None, #TODOchecked
+    }
+    val_rows.append(new_row)
+    counter += 1
+
+df = pd.concat([df, pd.DataFrame(val_rows)], ignore_index=True)
+
+
+# Process test rows
+counter = len(df)
+for _, row in ms_asl_test_json.iterrows():
+    category = row["clean_text"]
+
+    new_row = {
+        "id": counter + 1,
+        "category": category,
+        "dataset_type": "MSASL",
+        "url": row["url"],
+        "dataset_split": "", #TODOsplit 
+        "frame_start": row["start"],
+        "frame_end": row["end"],
+        "fps": row["fps"],
+        "filename": f"{counter+1}.mp4",
+        "is_valid": None, #TODOchecked
+        "is_duplicate": None, #TODOchecked
+    }
+    test_rows.append(new_row)
     counter += 1
 
 # Add all rows at once
-df = pd.concat([df, pd.DataFrame(rows)], ignore_index=True)
+df = pd.concat([df, pd.DataFrame(test_rows)], ignore_index=True)
 
 # Export to CSV
-df.to_csv("MSASL_test.csv", index=False)
+df.to_csv("MSASL.csv", index=False)
